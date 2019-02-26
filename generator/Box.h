504b850e66d72e79def453d8cf983146d9b59302
Filipe Monteiro <pimonteiro@protonmail.com>
2019-02-26 10:31:19 +0000

    3D Cube implement

---
+++ generator/Box.h
@@ -0,0 +1,15 @@
+//
+// Created by pimonteiro on 25-02-2019.
+//
+
+#ifndef CLASS2_BOX_H
+#define CLASS2_BOX_H
+#include <fstream>
+
+
+class Box {
+  public:
+    static void box(float x, float y, float z, int div, const std::string &filename);
+};
+
+#endif //CLASS2_BOX_H
