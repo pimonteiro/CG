f2aac5cb2321acbdafdafa6cd429a10e499b3729
anacatarinasousaaa <ana_96_sousa@hotmail.com>
2019-02-28 16:11:25 +0000

    Add cone generator

---
+++ generator/cone.h
@@ -0,0 +1,9 @@
+#ifndef __CONE_H__
+#define __CONE_H__
+
+#include <string>
+
+void cone(float, float, int, int, const std::string&);
+std::string frustum(float, float, int, float, float, float, int*);
+
+#endif
