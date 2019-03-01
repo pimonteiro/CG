12f5a172f627ad9b603664361722056faa2060f0
Filipe Monteiro <pimonteiro@protonmail.com>
2019-03-01 21:38:03 +0000

    fix herulume's nitpicking ideas

--- engine/Point.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by pimonteiro on 01-03-2019.
-//
-
-#include "Point.h"
---
+++ engine/point.h
@@ -0,0 +1,13 @@
+#ifndef POINT_H
+#define POINT_H
+
+
+class point {
+public:
+    float x;
+    float y;
+    float z;
+};
+
+
+#endif
