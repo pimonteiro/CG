a9992d15c910d255ae092e53ad965913461ead34
Eduardo Barbosa <herulume@protonmail.com>
2019-03-11 15:22:56 +0000

    Make model work with triangles

--- src/engine/headers/model.h
+++ src/engine/headers/model.h
@@ -3,16 +3,16 @@
 
 #include <string>
 #include <vector>
-#include "point.h"
+#include "triangle.h"
 
-class  Model {
-    std::vector<Point*> pointsV;
-    std::vector<Point*> colorsV;
-  public:
-    Model();
-    void addElement(Point*);
-    void addColor(Point*);
-    std::vector<Point*> model();
-    void draw();
+class Model {
+    private:
+        std::vector<Triangle*> triV;
+    public:
+        Model();
+        void addElement(Triangle*);
+        std::vector<Triangle*> model();
+        void draw();
 };
+
 #endif
