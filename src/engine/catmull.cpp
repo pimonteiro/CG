c5a8820a985983149cc15131f047cf9f707d0e11
Filipe Monteiro <pimonteiro@protonmail.com>
2019-04-13 14:34:47 +0100

    code cleanup

--- src/engine/catmull.cpp
+++ src/engine/catmull.cpp
@@ -52,13 +52,13 @@ Catmull:: setYAxis(float y[3])
 Point
 Catmull::getCatmullRomPoint(float t, Point p0, Point p1, Point p2, Point p3, float* deriv)
 {
-        float pp0[3] = {p0.X(), p0.Y(), p0.Z()};
-        float pp1[3] = {p1.X(), p1.Y(), p1.Z()};
-        float pp2[3] = {p2.X(), p2.Y(), p2.Z()};
-        float pp3[3] = {p3.X(), p3.Y(), p3.Z()};
+        float pp0[3] {p0.X(), p0.Y(), p0.Z()};
+        float pp1[3] {p1.X(), p1.Y(), p1.Z()};
+        float pp2[3] {p2.X(), p2.Y(), p2.Z()};
+        float pp3[3] {p3.X(), p3.Y(), p3.Z()};
         float ppos[3];
         // catmull-rom matrix
-        float m[4][4] = {	{-0.5f,  1.5f, -1.5f,  0.5f},
+        float m[4][4] {	{-0.5f,  1.5f, -1.5f,  0.5f},
                 { 1.0f, -2.5f,  2.0f, -0.5f},
                 {-0.5f,  0.0f,  0.5f,  0.0f},
                 { 0.0f,  1.0f,  0.0f,  0.0f}
@@ -66,24 +66,24 @@ Catmull::getCatmullRomPoint(float t, Point p0, Point p1, Point p2, Point p3, flo
         // Compute A = M * P
         float A[3][4];
 
-        for (int i = 0; i < 3; i++) {
-                float v[4] = {pp0[i], pp1[i], pp2[i], pp3[i]};
+        for (int i {0}; i < 3; i++) {
+                float v[4] {pp0[i], pp1[i], pp2[i], pp3[i]};
                 multMatrixVector(m[0], v, A[i]);
         }
 
         // Compute pos = T * A
-        float T[4] = {t*t * t, t * t, t, 1};
+        float T[4] {t*t * t, t * t, t, 1};
 
-        for (int i = 0; i < 3; i++) {
-                float v[4] = {A[i][0], A[i][1], A[i][2], A[i][3]};
+        for (int i { 0 }; i < 3; i++) {
+                float v[4] {A[i][0], A[i][1], A[i][2], A[i][3]};
                 ppos[i] = T[0] * v[0] + T[1] * v[1] + T[2] * v[2] + T[3] * v[3];
         }
 
         // compute deriv = T' * A
-        float TT[4] = {3 * t * t, 2 * t, 1, 0};
+        float TT[4] {3 * t * t, 2 * t, 1, 0};
 
-        for (int i = 0; i < 3; i++) {
-                float v[4] = {A[i][0], A[i][1], A[i][2], A[i][3]};
+        for (int i {0}; i < 3; i++) {
+                float v[4] {A[i][0], A[i][1], A[i][2], A[i][3]};
                 deriv[i] = TT[0] * v[0] + TT[1] * v[1] + TT[2] * v[2] + TT[3] * v[3];
         }
 
@@ -95,9 +95,9 @@ Catmull::getCatmullRomPoint(float t, Point p0, Point p1, Point p2, Point p3, flo
 Point
 Catmull::getGlobalCatmullRomPoint(float gt, float *deriv)
 {
-        int nP { this->contP.size()};
+        int nP { static_cast<int>(this->contP.size())};
         float t { gt * nP} ; // this is the real global t
-        int index { floor(t) };  // which segment
+        int index { static_cast<int>(floor(t)) };  // which segment
         t = t - index; // where within  the segment
         // indices store the points
         int indices[4];
