9dc400799720c4875549faab790812f7797b35a4
Filipe Monteiro <pimonteiro@protonmail.com>
2019-05-05 01:20:11 +0100

    add generation of vertex and normal coords

--- src/generator/headers/outputAux.h
+++ src/generator/headers/outputAux.h
@@ -7,7 +7,11 @@
 
 std::string writePoint(float, float, float);
 void dumpFile(const std::ostringstream &, const std::string &);
+<<<<<<< HEAD
 std::string writeVector(std::vector<Point *>);
 std::string writeTextVector(std::vector<Point *>);
+=======
+std::string writeVector(std::vector<Point>);
+>>>>>>> a2cb27f... add generation of vertex and normal coords
 
 #endif
