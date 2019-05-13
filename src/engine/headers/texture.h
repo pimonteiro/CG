630e53db5e0ba92cdfc73ad9ac4791546bf4f2f2
Filipe Monteiro <pimonteiro@protonmail.com>
2019-05-13 14:57:16 +0100

    refactor 2 texture

--- src/engine/headers/texture.h
+++ src/engine/headers/texture.h
@@ -4,19 +4,22 @@
 #include <vector>
 #include <string>
 #include "../../lib/headers/point.h"
+#include "material.h"
 
 class Texture {
     private:
         char* filename;
-        std::vector<float> pointArray;
         unsigned int imageWidth;
         unsigned int imageHeight;
+        Material *material;
     public:
         unsigned int textBuffer;
         Texture();
         ~Texture();
         void addFile(std::string);
-        void loadImage(int);
+        void addMaterial(Material *);
+        void setup();
+        void loadImage();
 };
 
 #endif
